43b0cdbf594a72ef5a1dcb2cf367e6b2b9fdface
royzhuo <731924600@qq.com>
2016-06-20 17:04:46 +0800

    add test3
    test3 file

---
+++ TestSqLite3/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  TestSqLite3
+//
+//  Created by Roy on 16/6/20.
+//  Copyright © 2016年 Roy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ TestSqLite3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestSqLite3
+//
+//  Created by Roy on 16/6/20.
+//  Copyright © 2016年 Roy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
